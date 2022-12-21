921adbea21def0bd7313f8b782344d01b9980b77
Dan Walker <dan@rowdypixel.com>
2022-12-21 09:39:05 -0500

    Initial Commit

---
+++ TyperventilateWriter/TyperventilateWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TyperventilateWriterApp.swift
+//  TyperventilateWriter
+//
+//  Created by Dan Walker on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct TyperventilateWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
